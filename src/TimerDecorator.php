657e33215dfa26e3ca6f5edeb4385ce871f15f7a
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-04-07 22:38:39 +0200

    Added docblocks to TimerDecorator

--- src/TimerDecorator.php
+++ src/TimerDecorator.php
@@ -2,6 +2,7 @@
 
 namespace WyriHaximus\React\Inspector;
 
+use React\EventLoop\LoopInterface;
 use React\EventLoop\Timer\TimerInterface;
 
 class TimerDecorator implements TimerInterface
@@ -18,43 +19,66 @@ class TimerDecorator implements TimerInterface
 
     /**
      * TimerDecorator constructor.
+     * @param LoopInterface $loop
+     * @param TimerInterface $timer
      */
-    public function __construct(LoopDecorator $loop, TimerInterface $timer)
+    public function __construct(LoopInterface $loop, TimerInterface $timer)
     {
         $this->loop  = $loop;
         $this->timer = $timer;
     }
 
+    /**
+     * @return LoopInterface
+     */
     public function getLoop()
     {
         return $this->loop;
     }
 
+    /**
+     * @return float
+     */
     public function getInterval()
     {
         return $this->timer->getInterval();
     }
 
+    /**
+     * @return callable
+     */
     public function getCallback()
     {
         return $this->timer->getCallback();
     }
 
+    /**
+     * @param $data mixed
+     */
     public function setData($data)
     {
-        return $this->timer->setData($data);
+        $this->timer->setData($data);
     }
 
+    /**
+     * @return mixed
+     */
     public function getData()
     {
         return $this->timer->getData();
     }
 
+    /**
+     * @return bool
+     */
     public function isPeriodic()
     {
         return $this->timer->isPeriodic();
     }
 
+    /**
+     * @return bool
+     */
     public function isActive()
     {
         return $this->timer->isActive();
